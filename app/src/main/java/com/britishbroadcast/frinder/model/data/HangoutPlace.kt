132dadd1df7f6ecc0a293c8f3c8f433c8279bc66
MindaRah <78925176+MindaRah@users.noreply.github.com>
2021-03-17 13:26:37 +0000

    Added entity name

--- app/src/main/java/com/britishbroadcast/frinder/model/data/HangoutPlace.kt
+++ app/src/main/java/com/britishbroadcast/frinder/model/data/HangoutPlace.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "hangouts" )
 data class HangoutPlace(
     val business_status: String,
     val geometry: Geometry,
